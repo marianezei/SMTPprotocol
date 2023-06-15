4d1796ff678db3a57d9be86aebe0b0a2893c2cf6
Huggo Parcelly <h.parcelly@gmail.com>
2023-06-14 22:05:52 -0300

    adiciona thread concorrentes

--- smpt.go
+++ smpt.go
@@ -2,25 +2,48 @@ package main
 
 import (
 		"fmt"
+		"time"
     "log"
     "net/smtp"
 )
 
-func main() {
-
+func sendEmail(emails []string, msg []byte) {
 	smtpHost := "smtp.gmail.com"
 	username := "projetoconcorrente@gmail.com"
 	password := "kesbrjebqslmkxos"
 
-    // Set up authentication information.
-    auth := smtp.PlainAuth("", username, password, smtpHost)
+  // Set up authentication information.
+  auth := smtp.PlainAuth("", username, password, smtpHost)
 
-    // Connect to the server, authenticate, set the sender and recipient,
-    // and send the email all in one step.
-    err := smtp.SendMail(smtpHost+":587", auth, username, []string{"h.parcelly@gmail.com"}, []byte("Este é um teste de e-mail enviado usando Go e o protocolo SMTP."))
+
+	err := smtp.SendMail(smtpHost+":587", auth, username, emails, msg)
     if err != nil {
         log.Fatal(err)
     }
+}
 
-		fmt.Println("E-mail enviado com sucesso!")
+func calma() {
+	for {
+			time.Sleep(time.Second * 1)
+			fmt.Printf("calma ezequias\n")
+	}
+}
+
+
+func main() {
+
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one step.
+	mails := []string{"h.parcelly@gmail.com"}
+	msg := []byte("Este é o PRIMEIRO teste de e-mail enviado usando Go e o protocolo SMTP.")
+
+	mails1 := []string{"ezequias.bernardo@ccc.ufcg.edu.br", "mariane.zeitouni@ccc.ufcg.edu.br", "huggo.silva@ccc.ufcg.edu.br", "h.parcelly@gmail.com"}
+	msg1 := []byte("Este é o SEGUNDO email de teste usando Go e o protocolo SMTP.")
+
+	go sendEmail(mails, msg)
+	go calma()
+	sendEmail(mails1, msg1)
+	
+
+	fmt.Println("E-mails enviados com sucesso!")
 }
